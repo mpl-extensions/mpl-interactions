484e7350df8959dbae16d7e024a7b2eb713c2841
ianhi <ianhuntisaak@gmail.com>
2020-08-04 21:23:23 -0400

    allow tuples to be used as arugments to linspace

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="mpl_interactions",
-    version="0.1",
+    version="0.2.0-dev",
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
