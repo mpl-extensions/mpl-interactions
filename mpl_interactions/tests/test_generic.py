402efdd2a1c9cf1144ef6015a016ff7e0f074d4c
ianhi <ianhuntisaak@gmail.com>
2020-09-04 14:26:55 -0400

    add pytest-mpl

---
+++ mpl_interactions/tests/test_generic.py
@@ -0,0 +1,34 @@
+import pytest
+import matplotlib.pyplot as plt
+from mpl_interactions.generic import *
+import matplotlib.cbook as cbook
+import numpy as np
+
+
+@pytest.mark.mpl_image_compare
+def test_heatmap_slicer():
+    x = np.linspace(0, np.pi, 100)
+    y = np.linspace(0, 10, 200)
+    X, Y = np.meshgrid(x, y)
+    data1 = np.sin(X) + np.exp(np.cos(Y))
+    data2 = np.cos(X) + np.exp(np.sin(Y))
+    fig, axes = heatmap_slicer(
+        x,
+        y,
+        (data1, data2),
+        slices="both",
+        heatmap_names=("dataset 1", "dataset 2"),
+        labels=("Some wild X variable", "Y axis"),
+        interaction_type="move",
+    )
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_image_segmentation():
+    with cbook.get_sample_data("ada.png") as image_file:
+        image = plt.imread(image_file)
+    mask = np.load("../../examples/ada-mask.npy")
+    preloaded = image_segmenter(image, nclasses=3, mask=mask)
+
+    return preloaded.fig
--- setup.py
+++ setup.py
@@ -1,68 +1,70 @@
 from setuptools import setup, find_packages
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # extract version
-path = path.realpath('mpl_interactions/_version.py')
+path = path.realpath("mpl_interactions/_version.py")
 version_ns = {}
 with open(path, encoding="utf8") as f:
     exec(f.read(), {}, version_ns)
-version = version_ns['__version__']
+version = version_ns["__version__"]
 
 setup(
     name="mpl_interactions",
     version=version,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires = [
-        'ipywidgets>=7.5.0,<8',
-        'matplotlib',
-        'ipympl>=0.5.7',
-        'packaging',
+    install_requires=[
+        "ipywidgets>=7.5.0,<8",
+        "matplotlib",
+        "ipympl>=0.5.7",
+        "packaging",
     ],
-    author          = 'Ian Hunt-Isaak',
-    author_email    = 'ianhuntisaak@gmail.com',
-    license         = 'BSD',
-    platforms       = "Linux, Mac OS X, Windows",
-    description     = 'Matplotlib aware interact functions',
-    keywords        = ['Jupyter', 'Widgets', 'IPython', 'Matplotlib'],
-    classifiers     = [
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Framework :: Jupyter',
-        'Framework :: Matplotlib'
+    author="Ian Hunt-Isaak",
+    author_email="ianhuntisaak@gmail.com",
+    license="BSD",
+    platforms="Linux, Mac OS X, Windows",
+    description="Matplotlib aware interact functions",
+    keywords=["Jupyter", "Widgets", "IPython", "Matplotlib"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Framework :: Jupyter",
+        "Framework :: Matplotlib",
     ],
-    url = 'https://github.com/ianhi/mpl-interactions',
-    extras_require = {
-        'docs': [
-            'sphinx>=1.5',
-            'mock',
-            'numpydoc',
-            'recommonmark',
-            'sphinx_rtd_theme',
-            'nbsphinx>=0.2.13,<0.4.0',
-            'jupyter_sphinx',
-            'pytest_check_links',
-            'pypandoc',
-            'jupyter-sphinx',
-            'sphinx-copybutton',
-            'sphinx-autobuild',
+    url="https://github.com/ianhi/mpl-interactions",
+    extras_require={
+        "docs": [
+            "sphinx>=1.5",
+            "mock",
+            "numpydoc",
+            "recommonmark",
+            "sphinx_rtd_theme",
+            "nbsphinx>=0.2.13,<0.4.0",
+            "jupyter_sphinx",
+            "pytest_check_links",
+            "pypandoc",
+            "jupyter-sphinx",
+            "sphinx-copybutton",
+            "sphinx-autobuild",
         ],
-        'test': [
-            'pytest',
-            'nbval',
-            'PyQt5',
-            'black',
+        "test": [
+            "pytest",
+            "pytest-mpl",
+            "nbval",
+            "PyQt5",
+            "black",
         ],
-    }
+    },
 )
