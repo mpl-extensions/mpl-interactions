db6cbd24154211ad2b58a8138cf2cccd2a08d409
ianhi <ianhuntisaak@gmail.com>
2020-09-12 01:03:54 -0400

    create broadcasting helper functions
    Basically a nightmarish reimplementation of a subset of numpy broadcasting. Reimplemented such that it only considers the first axis and allows the other axes to match

--- mpl_interactions/helpers.py
+++ mpl_interactions/helpers.py
@@ -1,9 +1,17 @@
 import numpy as np
+from numpy.distutils.misc_util import is_sequence
+from numbers import Number
+from collections.abc import Callable
 
 __all__ = [
     "decompose_bbox",
     "update_datalim_from_xy",
     "update_datalim_from_bbox",
+    "is_jagged",
+    "broadcast_to",
+    "prep_broadcast",
+    "broadcast_arrays",
+    "broadcast_many",
 ]
 
 
@@ -49,3 +57,92 @@ def update_datalim_from_xy(ax, x, y, stretch_x=True, stretch_y=True):
     y0_ = np.min(y)
     y1_ = np.max(y)
     _update_limits(ax, *decompose_bbox(ax.dataLim), x0_, y0_, x1_, y1_, stretch_x, stretch_y)
+
+
+def is_jagged(seq):
+    """
+    checks for jaggedness up to two dimensions
+    don't need more because more doesn't make any sense for this library
+    need this bc numpy is unhappy about being passed jagged arrays now :(
+    """
+    lens = []
+    if is_sequence(seq):
+        for y in seq:
+            if isinstance(y, Number) or isinstance(y, Callable):
+                lens.append(0)
+                continue
+            try:
+                lens.append(len(y))
+            except TypeError:
+                return True
+        if not all(lens[0] == l for l in lens):
+            return True
+    return False
+
+
+def prep_broadcast(arr):
+    if is_jagged(arr):
+        arr = np.atleast_2d(np.asarray(arr, dtype=np.object))
+    else:
+        arr = np.asarray(arr)
+        if np.issubdtype(arr.dtype, np.number) and arr.ndim == 1:
+            arr = arr[None, :]
+    return arr
+
+
+def prep_broadcast(arr):
+    if arr is None:
+        return np.array([None])
+    if is_jagged(arr):
+        arr = np.asarray(arr, dtype=np.object)
+    else:
+        arr = np.atleast_1d(arr)
+        if np.issubdtype(arr.dtype, np.number) and arr.ndim == 1:
+            arr = arr[None, :]
+    return arr
+
+
+def broadcast_to(arr, to_shape, names):
+    """
+    happily this doesn't increase memory footprint e.g:
+    import sys
+    xs = np.arange(5)
+    print(sys.getsizeof(xs.nbytes))
+    print(sys.getsizeof(np.broadcast_to(xs, (19000, xs.shape[0]))))
+
+    gives 28 and 112. Note 112/28 != 19000
+    """
+    if arr.ndim == 2:
+        if arr.shape[0] == 1:
+            return np.broadcast_to(arr, (to_shape[0], arr.shape[1]))
+        elif arr.shape[0] == to_shape[0]:
+            return arr
+        else:
+            raise ValueError(f"can't broadcast {names[0]} {arr.shape} onto {names[1]} {to_shape}")
+    elif arr.ndim > 2:
+        raise ValueError(f"{names[0]} can't have dimension > 2, it has {arr.ndim} dimensions")
+    else:
+        return np.broadcast_to(arr, (to_shape[0], arr.shape[0]))
+
+
+def broadcast_arrays(*args):
+    """
+    This is a modifed version the numpy `broadcast_arrays` function
+    that uses a verion of _broadcast_to that only considers the first axis
+    """
+
+    shapes = [array.shape[0] for (array, name) in args]
+    idx = np.argmax(shapes)
+    if all([shapes[0] == s for s in shapes]):
+        # case where nothing needs to be broadcasted.
+        return [array for (array, name) in args]
+    return [broadcast_to(array, args[idx][0].shape, [name, args[idx][1]]) for (array, name) in args]
+
+
+def broadcast_many(*args):
+    """
+    helper to call prep_broadcast followed by broadcast arrays
+    keep as a separate function to keep the idea of broadcast_arrays the same
+    """
+    ret = broadcast_arrays(*[(prep_broadcast(arg[0]), arg[1]) for arg in args])
+    return ret
