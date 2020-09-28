4d669b76ce3579133d52667e89a1bb1cd95c6b78
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2020-09-28 19:51:52 -0400

    create ipyplot module
    usage: `import mpl_interactions.ipyplot as iplt`

---
+++ mpl_interactions/ipyplot.py
@@ -0,0 +1,4 @@
+from .pyplot import interactive_hist as hist
+from .pyplot import interactive_plot as plot
+from .pyplot import interactive_imshow as imshow
+from .pyplot import interactive_scatter as scatter
