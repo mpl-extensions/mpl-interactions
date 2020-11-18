c83611b263edf565987cb265278ebd265f27f192
John Russell <johncrussell25@gmail.com>
2020-11-18 13:51:36 -0500

    support xarray and dask in hyperslicer

--- mpl_interactions/generic.py
+++ mpl_interactions/generic.py
@@ -15,6 +15,7 @@ from packaging import version
 from .helpers import *
 from .utils import figure, ioff, nearest_idx
 from .controller import gogogo_controls
+from .xarray_helpers import *
 
 # functions that are methods
 __all__ = [
@@ -577,8 +578,16 @@ def hyperslicer(
     -------
     controls
     """
+    
+    arr = np.squeeze(arr)
+    
+    arr_type = 'numpy'
+    if  'xarray.core.dataarray.DataArray' in str(arr.__class__):
+        arr_type = 'xarray'
+    elif 'dask.array.core.Array' in str(arr.__class__):
+        arr_type = 'dask'
+    
 
-    arr = np.asarray(np.squeeze(arr))
     if arr.ndim < 3 + is_color_image:
         raise ValueError(
             f"arr must be at least {3+is_color_image}D but it is {arr.ndim}D. mpl_interactions.imshow for 2D images."
@@ -599,11 +608,15 @@ def hyperslicer(
 
     names = None
     axes = None
-    if "names" in kwargs:
-        names = kwargs.pop("names")
+    if arr_type != 'xarray':
+        if "names" in kwargs:
+            names = kwargs.pop("names")
 
-    elif "axes" in kwargs:
-        axes = kwargs.pop("axes")
+        elif "axes" in kwargs:
+            axes = kwargs.pop("axes")
+    
+    else:
+        axes = get_hs_axes(arr, is_color_image=is_color_image)
 
     # Just pass in an array - no kwargs
     for i in range(arr.ndim - im_dims):
@@ -661,7 +674,14 @@ def hyperslicer(
         if not name in kwargs:
             slider_format_strings[name] = "{:.0f}"
             kwargs[name] = np.arange(arr.shape[i])
-
+    
+    if arr_type =='xarray':
+        slider_format_strings = get_hs_fmts(arr, is_color_image=is_color_image)
+        extent = get_hs_extent(arr, is_color_image=is_color_image) 
+    else:
+        if 'extent' not in kwargs:
+            extent=None
+   
     controls, params = gogogo_controls(
         kwargs, controls, display_controls, slider_format_strings, play_buttons, allow_dupes=True
     )
