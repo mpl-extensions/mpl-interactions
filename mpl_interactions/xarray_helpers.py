035455c3e276333ee664fcab80b9e0b7182a6d9c
John Russell <johncrussell25@gmail.com>
2020-11-18 14:28:19 -0500

    docstrings for xarray_helpers

--- mpl_interactions/xarray_helpers.py
+++ mpl_interactions/xarray_helpers.py
@@ -1,6 +1,23 @@
 import numpy as np
 
 def choose_fmt_str(dtype=None):
+    """
+    Choose the appropriate string formatting for different dtypes.
+    
+    Paramters
+    ---------
+    
+    dtype: np.dtye
+        Dtype of array containing values to be formatted.
+        
+        
+    Returns
+    -------
+    
+    fmt: str
+        Format string
+        
+    """
     if np.issubdtype(dtype,'float'):
         fmt = r'{:0.2f}'
     elif np.issubdtype(dtype,'int'):
@@ -11,6 +28,22 @@ def choose_fmt_str(dtype=None):
 
 
 def choose_datetime_nonsense(arr, timeunit='m'):
+    """
+    Try to do something reasonable to datetimes and timedeltas.
+    
+    Parameters
+    ----------
+    
+    arr: np.array 
+        Array with values to be formatted.
+    
+    Returns
+    -------
+    
+    out: np.array
+        Array modified to format decently in a slider.
+        
+    """
     if np.issubdtype(arr.dtype, 'datetime64'):
         #print('datetime')
         out = arr.astype(f'datetime64[{timeunit}]')
@@ -22,6 +55,28 @@ def choose_datetime_nonsense(arr, timeunit='m'):
 
 
 def get_hs_axes(xarr, is_color_image=False, timeunit='m'):
+    """
+    Read the dims and coordinates from an xarray and construct the 
+    axes argument for hyperslicer. Called internally by hyperslicer.
+    
+    Parameters
+    ----------
+    
+    xarr: xarray.DataArray
+        DataArray being viewed with hyperslicer
+        
+    is_color_image: bool default False
+        Whether the individual images of the hyperstack are color images.
+        
+    timeunit: str default "m"
+        Truncation level for datetime and timedelta axes. 
+        
+    Returns
+    -------
+    axes: list
+        axes kwarg for hyperslicer
+        
+    """
     if not is_color_image:
         dims = xarr.dims[:-2]
     else: 
@@ -33,6 +88,26 @@ def get_hs_axes(xarr, is_color_image=False, timeunit='m'):
 
 
 def get_hs_extent(xarr, is_color_image=False):
+    """
+    Read the "XY" coordinates of an xarray.DataArray to set extent of image for 
+    imshow.
+    
+    Parameters
+    ----------
+    
+    xarr: xarray.DataArray
+        DataArray being viewed with hyperslicer
+        
+    is_color_image: bool default False
+        Whether the individual images of the hyperstack are color images.
+        
+    Returns
+    -------
+    extent: list
+        Extent argument for imshow. [d0_min, d0_max, d1_min, d1_max]
+    
+    """
+    
     if not is_color_image:
         dims = xarr.dims[-2:]
     else:
@@ -46,6 +121,28 @@ def get_hs_extent(xarr, is_color_image=False):
 
 
 def get_hs_fmts(xarr, units=None, is_color_image=False):
+    """
+    Get appropriate slider format strings from xarray coordinates 
+    based the dtype of corresponding values.
+    
+    Parameters
+    ----------
+    
+    xarr: xarray.DataArray
+        DataArray being viewed with hyperslicer
+    
+    units: array-like
+        Units to append to end of slider value. Must have the same length 
+        as number of non-image dimensions in xarray.
+    
+    is_color_image: bool default False
+        Whether the individual images of the hyperstack are color images.
+        
+    Returns
+    -------
+    fmt_strings: dict
+        Slider format strings for hyperslicer (or other mpl-interactions?)
+    """
     if not is_color_image:
         dims = xarr.dims[:-2]
     else:
