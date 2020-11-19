e67d537f5f44e41733f86e9d7542182edd2e9f13
John Russell <johncrussell25@gmail.com>
2020-11-18 19:34:00 -0500

    move choose_fmt_str to helpers

--- mpl_interactions/helpers.py
+++ mpl_interactions/helpers.py
@@ -627,3 +627,31 @@ def gogogo_display(ipympl, use_ipywidgets, display, controls, fig):
             fig.show()
             controls[0].show()
     return controls
+
+def choose_fmt_str(dtype=None):
+    """
+    Choose the appropriate string formatting for different dtypes.
+    
+    Paramters
+    ---------
+    
+    dtype : np.dtye
+    dtype of array containing values to be formatted.
+                                                
+    Returns
+    -------
+                                                        
+    fmt : str
+    Format string
+    """
+    if np.issubdtype(dtype,'float'):
+        fmt = r'{:0.2f}'
+    
+    elif np.issubdtype(dtype,'int'):
+        fmt = r'{:d}'
+    
+    else:
+        fmt =r'{:}'
+    
+    return fmt
+      
