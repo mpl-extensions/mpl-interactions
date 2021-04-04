b08e3933dcd60db43551c4c6b90055d64566b56e
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-04-03 21:58:25 -0400

    Fix controls test

--- mpl_interactions/tests/test_controls.py
+++ mpl_interactions/tests/test_controls.py
@@ -12,5 +12,11 @@ def test_eager_register():
     assert len(zoop) == 1
 
     ctrls = Controls(beep=(0, 1), boop=(0, 1))
-    ctrls.register_callback(cb, "beep", eager=True)
+    ctrls.register_callback(cb, "beep", eager=False)
     assert len(zoop) == 1
+
+    ctrls.controls["beep"].set_val(0.5)
+    assert len(zoop) == 2
+
+    ctrls.controls["boop"].set_val(0.5)
+    assert len(zoop) == 2
